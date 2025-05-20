d973f9accd34578a47c76f540efa5d55c1f0917a
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2025-05-20 19:11:06 +0200

    Feat: add bonus tracks and cars

--- pkgs/rvgl-bin.nix
+++ pkgs/rvgl-bin.nix
@@ -2,6 +2,7 @@
 , pkgs
 , stdenv
 , fetchFromGitLab
+, flac
 , SDL2
 , SDL2_image
 , openal
@@ -9,7 +10,11 @@
 , libunistring
 , makeWrapper
 }:
-
+let car_skins = true;
+    additional_cars = true;
+    additional_tracks = true;
+    bonus_tracks = true;
+in
 stdenv.mkDerivation (finalAttrs: rec {
   pname = "rvgl-bin";
   version = "";
@@ -48,17 +53,87 @@ stdenv.mkDerivation (finalAttrs: rec {
       rev = "23.1030a1";
       hash = "sha256-OlCNBUbyu/hA75qk27xSldjKXsPyaGLXxthtogdmfkQ=";
     })
-  ];
+
+    (fetchFromGitLab rec {
+      name = repo;
+      owner = "re-volt";
+      repo = "rvio/cars";
+      rev = "25.0414";
+      hash = "sha256-/BkBwzwRwAorz4FghCCN7R8E7cbWwrQqNAbz0e0gcYk=";
+    })]
+
+    # skins
+    ++ (lib.optional car_skins [
+
+      (fetchFromGitLab rec {
+        name = repo;
+        owner = "re-volt";
+        repo = "rvio/skins";
+        rev = "25.0414";
+        hash = "sha256-rsRcsTy3tAUgf8BrVPceTA/S7jfaFiJn2XyvIkTkttg=";
+      })
+    ])
+
+    # bonus cars
+    ++ (lib.optional additional_cars [
+
+      (fetchFromGitLab rec {
+        name = repo;
+        owner = "re-volt";
+        repo = "rvio/bonus_cars";
+        rev = "25.0414";
+        hash = "sha256-tf39G4MAGRWmnTx5k4RV69BwqtsBnjqe2BJrRA1SXZk=";
+      })
+
+      (fetchFromGitLab rec {
+        name = repo;
+        owner = "re-volt";
+        repo = "rvio/bonus_skins";
+        rev = "25.0414";
+        hash = "sha256-hOBXFahGuOx1upoUngrvIBFWs+a7EJq35usAzLt8O3s=";
+      })
+    ])
+
+    # tracks
+    ++ (lib.optional additional_tracks [
+
+      (fetchFromGitLab rec {
+        name = repo;
+        owner = "re-volt";
+        repo = "rvio/tracks";
+        rev = "25.0414";
+        hash = "sha256-7Ex2p9/3kZ5vpQTNwsvmd33ehRKQZUVXfvmePcPP0N0=";
+      })
+    ])
+
+    # bonus tracks
+    ++ (lib.optional bonus_tracks [
+      (fetchFromGitLab rec {
+        name = repo;
+        owner = "re-volt";
+        repo = "rvio/bonus_tracks";
+        rev = "25.0414";
+        hash = "sha256-RRBinXyRJ9nOPLBNexo6D89qXXy0yjZjB2cPrTI4vqI=";
+      })
+    ])
+  ;
 
 
+  # TODO: adapt wrt flags
   installPhase = ''
     #echo "$PWD"
-    #ls -al
+    ls -al
     #ls -al $srcs
     mkdir -p $out/opt/rvgl
-    cp -a rvgl-assets/ $out/opt/rvgl
-    cp -a game_files/ $out/opt/rvgl
+    cp -R rvgl-assets/* $out/opt/rvgl
+    cp -R game_files/* $out/opt/rvgl
     cp -a rvgl-platform/linux/ $out/opt/rvgl
+    cp -R rvio-cars/* $out/opt/rvgl
+    cp -R rvio-skins/* $out/opt/rvgl
+    cp -R rvio-bonus_cars/* $out/opt/rvgl
+    cp -R rvio-bonus_skins/* $out/opt/rvgl
+    cp -R rvio-tracks/* $out/opt/rvgl
+    cp -R rvio-bonus_tracks/* $out/opt/rvgl
 
     cp -a rvgl-platform/linux/rvgl.64 $out/opt/rvgl/rvgl
 
@@ -68,11 +143,14 @@ stdenv.mkDerivation (finalAttrs: rec {
 
     patchelf --replace-needed "libunistring.so.2" "libunistring.so.5" "$out/opt/rvgl/rvgl"
 
+    patchelf --set-rpath $out/opt: "$out/opt/rvgl/rvgl"
+
       wrapProgram $out/opt/rvgl/rvgl                                \
             --prefix LD_LIBRARY_PATH : $libPath \
     '';
 
     libPath = lib.makeLibraryPath [
+      flac
       SDL2
       SDL2_image
       openal
