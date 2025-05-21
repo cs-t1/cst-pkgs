77ea979d1bb28ecb2e8f2d22993ac10ff01f16e4
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2025-05-21 16:38:17 +0200

    flake-utils

--- flake.nix
+++ flake.nix
@@ -4,8 +4,14 @@
   # Externally extensible flake systems. See <https://github.com/nix-systems/nix-systems>.
   inputs.systems.url = "github:nix-systems/default";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, systems, nixpkgs }: {
-    pkgs = import ./default.nix { pkgs = (import nixpkgs { }); };
-  };
+  outputs = { self, systems, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        {
+
+          pkgs = import ./default.nix { pkgs = (import nixpkgs { }); };
+        };
+        )
 }
